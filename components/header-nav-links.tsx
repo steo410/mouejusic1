60dfa209e32d515ba064ced2f00698d3a8a8e6c3
steo410 <steojhukna@gmail.com>
2026-04-03 14:22:52 +0900

    Create header-nav-links.tsx

---
+++ components/header-nav-links.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Me = {
+  user: null | { id: string; username: string; nickname: string; isAdmin?: boolean };
+};
+
+export function HeaderNavLinks() {
+  const [me, setMe] = useState<Me>({ user: null });
+
+  useEffect(() => {
+    async function load() {
+      const res = await fetch("/api/me");
+      setMe(await res.json());
+    }
+    load();
+  }, []);
+
+  return (
+    <div className="flex gap-4">
+      <Link href="/">홈</Link>
+      {!me.user && <Link href="/auth">로그인/회원가입</Link>}
+      {me.user && <Link href="/mypage">마이페이지</Link>}
+      {me.user?.isAdmin && <Link href="/admin">관리자</Link>}
+    </div>
+  );
+}
